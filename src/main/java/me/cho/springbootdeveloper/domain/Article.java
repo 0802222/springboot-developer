b08a3c444ccd2a992444a89dae72391c7a03eac7
0802222 <0802222@naver.com>
2026-02-06 12:46:19 +0900

    (0206) feat : Thymeleaf 학습

--- src/main/java/me/cho/springbootdeveloper/domain/Article.java
+++ src/main/java/me/cho/springbootdeveloper/domain/Article.java
@@ -2,17 +2,23 @@ package me.cho.springbootdeveloper.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id
@@ -37,4 +43,12 @@ public class Article {
         this.content = content;
     }
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    
 }
---
+++ src/main/java/me/cho/springbootdeveloper/dto/ArticleViewResponse.java
@@ -0,0 +1,24 @@
+package me.cho.springbootdeveloper.dto;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.cho.springbootdeveloper.domain.Article;
+
+@Getter
+@NoArgsConstructor
+public class ArticleViewResponse {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+
+}
