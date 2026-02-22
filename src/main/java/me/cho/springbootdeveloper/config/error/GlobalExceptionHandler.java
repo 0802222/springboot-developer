ac283b1e12f25b0942e9777e6cec527218b2a9b8
0802222 <0802222@naver.com>
2026-02-23 03:18:40 +0900

    (0223) fix : 예외처리 추가 (MethodArgumentNotValidException)

--- src/main/java/me/cho/springbootdeveloper/config/error/GlobalExceptionHandler.java
+++ src/main/java/me/cho/springbootdeveloper/config/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import me.cho.springbootdeveloper.config.error.exception.BusinessBaseException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,6 +32,12 @@ public class GlobalExceptionHandler {
         log.error("Exception", e);
 
         return createErrorResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException", e);
+        return createErrorResponseEntity(ErrorCode.INVALID_INPUT_VALUE);
     }
 
     private ResponseEntity<ErrorResponse> createErrorResponseEntity(ErrorCode errorCode) {
